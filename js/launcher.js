6bd9a4e5ad5b86e2201c899985fda2a08aabf2bc
vansh446 <81295978+vansh446@users.noreply.github.com>
2021-07-25 11:53:05 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,36 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+    this.sling.bodyA=null;
+       
+      }
+      attach(body){
+        this.sling.bodyA=body;
+           
+          }
+    
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(5);
+            line(pointA.x, pointA.y, pointB.x, pointB.y)
+
+        }
+        
+    }
+    
+    
+}
